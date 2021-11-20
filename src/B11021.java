e3f063e2e3b4e26407773e2a288fc315659e2cce
jaeseok <jssjchl@naver.com>
2021-11-21 03:56:05 +0900

    11-21

--- src/B11021.java
+++ src/B11021.java
@@ -8,11 +8,11 @@ public class B11021 {
         int t = Integer.parseInt(br.readLine());
 
         for(int i = 0; i <t; i ++){
-            String c= "Case #";
             String s = br.readLine();
             int A =Integer.parseInt(s.split(" ")[0]); // 문자열을 개행을 기준으로 spilt를 통해 나누어준다. 이 떼 첫 번째 자리[0]
             int B =Integer.parseInt(s.split(" ")[1]); //두 번째 자리 [2]
-            bw.write( c + (i+1)+ ": "+ (A+B) +"\n");
+            int C = A+B;
+            bw.write( "Case #" + (i+1)+ ": "+ A +" + "+B+ " = " + C +"\n");
 
         }
         bw.flush();
