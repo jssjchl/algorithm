8d421dd1c443470a1a311c9fa8915ddbca6b109a
jaeseok <jssjchl@naver.com>
2021-12-10 03:47:52 +0900

    merge

--- src/B2577.java
+++ src/B2577.java
@@ -1,16 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
-public class B2577 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(r.readLine());
-        int b = Integer.parseInt(r.readLine());
-        int c = Integer.parseInt(r.readLine());
+class b2577 {
 
-        int v = a *b*c;
-        String s = String.valueOf(v);
+    public static void main(String[] args) {
 
+        int str[] = new int[10];
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); // A
+        int b = sc.nextInt(); // B
+        int c = sc.nextInt(); // C
+        int result = a * b * c;
+
+        String ptr = String.valueOf(result);
+
+        for (int i = 0; i < ptr.length(); i++) {
+            str[ptr.charAt(i) - 48]++; //48혹은 '0'을 빼야한다 이유는 아스키코드
+        }
+        // str[] 의 속에 ptr.charAt(i)를 통해 대응하는 값에 인덱스에 ++로 인덱스값을 증가시킨다.
+        //
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
     }
 }
