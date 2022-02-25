8fda2ffa660891b97f720a3ecd4d1dfe4b0dcab9
jaeseok <jssjchl@naver.com>
2022-02-26 01:39:33 +0900

    fibonacci

---
+++ src/Fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static int Fibonacci(int n) {
+        if(n ==0)
+            return 0;
+        if(n==1)
+            return 1;
+        return Fibonacci(n-1) + Fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Fibonacci(n));
+    }
+}
