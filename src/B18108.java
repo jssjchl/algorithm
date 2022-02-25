d43db329b7634103c267ed8dd3d94bef4424c5c0
jaeseok <jssjchl@naver.com>
2022-02-26 01:53:59 +0900

    10926, 18108

---
+++ src/B18108.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class B18108 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        try {
+            if (n >= 1000 && n <= 3000) {
+                System.out.println(n - 543);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
