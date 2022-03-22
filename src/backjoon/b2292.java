81898920e66df89997e90458ca12fb41b5da3464
jaeseok <jssjchl@naver.com>
2022-03-23 03:19:14 +0900

    b2292

--- src/backjoon/b2292.java
+++ src/backjoon/b2292.java
@@ -1,4 +1,23 @@
 package backjoon;
 
+import java.util.Scanner;
+
 public class b2292 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = 2;
+        int c = 1;
+        if (n == 1) {
+            System.out.println(1);
+        } else {
+            while (r <= n) {
+                r = r + (6 * c);
+                c++;
+            }
+            System.out.println(c);
+        }
+    }
+
 }
