ec90dcbd562b217a4fa0084960a9faf1f1583ce0
jaeseok <jssjchl100@gmail.com>
2022-06-28 00:27:19 +0900

    test

--- src/Codility/StremyT1.java
+++ src/Codility/StremyT1.java
@@ -4,5 +4,6 @@ public class StremyT1 {
     public String solution(String S) {
         String answer = S.replace("minus", "-").replace("plus", "+");
         return answer;
+        //깃허브 테스트
     }
 }
