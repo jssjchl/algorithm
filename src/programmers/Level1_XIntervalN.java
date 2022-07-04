42f0830842aa4cee76e48ea3fd9d06e38d2ab5bf
jssjchl@naver.com <jssjchl@naver.com>
2022-07-04 23:33:56 +0900

    x만큼 증가하는 n개의 배열

---
+++ src/programmers/Level1_XIntervalN.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Level1_XIntervalN {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for (int i = 1; i <=n; i++) {
+            answer[i-1] = x * i;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Level1_XIntervalN a = new Level1_XIntervalN();
+        System.out.println(a.solution(2, 5));
+    }
+}
