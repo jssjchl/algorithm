48ebdc085260b87c8efeb3e0cbfc09117be48b9e
jssjchl100@gmail.com <jssjchl100@gmail.com>
2022-07-11 22:58:38 +0900

    레벨1 작은수 제거하기

--- src/programmers/Level1SmallNumDelete.java
+++ src/programmers/Level1SmallNumDelete.java
@@ -1,30 +1,33 @@
 package programmers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Level1SmallNumDelete {
     public int[] solution(int[] arr) {
         int[] answer = new int[arr.length - 1];
-
-        if (arr.length < 1) {
-            answer[0] = -1;
-        }
-        int min = Integer.MAX_VALUE;
-        for (int a : arr) {
-            if (min > a) {
-                min = a;
+        Arrays.sort(arr);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[0] != arr[i]) {
+                list.add(arr[i]);
             }
         }
-        int index = 0;
-        for (int a : arr) {
-            if (min != a) answer[index++] = a;
+        if (list.size() < 1) {
+            answer = new int[1];
+            answer[0] = -1;
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                answer[i] = list.get(i);
+            }
         }
         return answer;
     }
 
     public static void main(String[] args) {
         Level1SmallNumDelete a = new Level1SmallNumDelete();
-        int[] arr = {4, 3, 2, 1};
+        int[] arr = {3, 4, 1, 2};
         System.out.println(a.solution(arr));
+
     }
 }
