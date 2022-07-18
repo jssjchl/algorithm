9f668490d88cfb1b6af95af7571d368a2dc5a525
jaeseok <jssjchl100@gmail.com>
2022-07-19 00:45:53 +0900

    레벨1 정수내림차순

---
+++ src/programmers/Level1_IntegerDesc.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Level1_IntegerDesc {
+    public long solution(long n) {
+        String[] arr = String.valueOf(n).split("");
+        Integer arr2[] = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = Integer.parseInt(arr[i]);
+        }
+        Arrays.sort(arr2, Collections.reverseOrder());
+        String answer = "";
+        for (int i = 0; i < arr2.length; i++) {
+            answer += String.valueOf(arr2[i]);
+        }
+        return Long.parseLong(answer); //long이 아니라 integer로 변환하면 런타임 에러뜸..
+    }
+
+    public static void main(String[] args) {
+        Level1_IntegerDesc a = new Level1_IntegerDesc();
+
+        System.out.println(a.solution(118372));
+    }
+}
